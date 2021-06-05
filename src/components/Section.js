58a49888ea579a600937f7281a69d332866cb6f6
fede <federico.hammond8@gmail.com>
2021-06-05 18:08:30 -0300

    add image files, working on home

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,11 +1,71 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Section() {
     return (
-        <div>
-            
-        </div>
+        <Wrap>
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+            <DownArrow src="images/down-arrow.svg">
+            </DownArrow>
+        </Wrap>
     )
 }
 
 export default Section
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('images/model-3.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center; 
+`
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+
+`
+
+const RightButton = styled(LeftButton)`
+    
+`
+
+const DownArrow = styled.div`
+    margin-top: 20px;
+    height: 40px;
+`
