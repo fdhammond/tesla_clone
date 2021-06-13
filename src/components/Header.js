6125eee6db929151161ebfae91cd4712ba46e251
Federico Hammond <federico.hammond8@gmail.com>
2021-06-13 18:02:18 -0300

    redux part, rout the menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import MenuIcon from '@material-ui/icons/Menu'
 import CloseIcon from '@material-ui/icons/Close'
-import Fade from 'react-reveal/Fade';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 // 1.49
 
 function Header() {
     const [burgerStatus, setBurgerStatus] = useState(false);
+    const cars = useSelector(selectCars);
 
     return (
        <Container>
@@ -15,21 +17,25 @@ function Header() {
                <img src="/images/logo.svg"/>
            </a>
             <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
-                <a href="#">Model Y</a>
+                { cars && cars.map((car, index) => (
+                    <a key={index} href="#">{car}</a>
+                ))}
             </Menu>
 
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <CustomMenu />                
+                <CustomMenu onClick={ ()=> setBurgerStatus(true) } />                
             </RightMenu>
-            <BurgerNav>
+            <BurgerNav show={ burgerStatus }>
                 <CloseWrapper>
-                    <CustomClose />
+                    <CustomClose onClick={ ()=> setBurgerStatus(false) }/>
                 </CloseWrapper>
+                
+                { cars && cars.map((car, index) => (
+                    <li key={index}><a href="#">{car}</a></li>
+                ))}
+
                 <li><a href="#">Existing Inventory</a></li>
                 <li><a href="#">Used Inventory</a></li>
                 <li><a href="#">Trade-in </a></li>
@@ -110,6 +116,8 @@ const BurgerNav = styled.div`
     display: flex;
     flex-direction: column;
     text-align: start;
+    transform: ${ props => props.show ? 'translateX(0)' : 'translateX(100%)' };
+    transition: transform 0.2s;
     li {
         padding: 15px 0;
         border-bottom: 1px solid rgba(0, 0, 0, .2);
